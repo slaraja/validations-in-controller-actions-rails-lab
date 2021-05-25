3058b2572d3a3e5dd9e34563ddffa842f31f3134
Stephanie La Raja <stephanielaraja@gmail.com>
2021-05-25 09:40:06 -0400

    commit tree 9513acc7e5e601dc243609acdd1e96d267166253

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,11 +6,33 @@ class AuthorsController < ApplicationController
   def new
   end
 
-  def create
-    @author = Author.create!(author_params)
+  # def create
+  #   @author = Author.create!(author_params)
 
-    redirect_to author_path(@author)
+  #   redirect_to author_path(@author)
+  # end
+
+  def create
+    # Create a brand new, unsaved, not-yet-validated Post object from the form.
+    @author = Author.new(author_params)
+
+    # Run the validations WITHOUT attempting to save to the database, returning
+    # true if the Post is valid, and false if it's not.
+    if @author.valid?
+      # If--and only if--the post is valid, do what we usually do.
+      @author.save
+      # This returns a status_code of 302, which instructs the browser to
+      # perform a NEW REQUEST! (AKA: throw @post away and let the show action
+      # worry about re-reading it from the database)
+      redirect_to author_path(@author)
+    else
+      # If the post is invalid, hold on to @post, because it is now full of
+      # useful error messages, and re-render the :new page, which knows how
+      # to display them alongside the user's entries.
+      render :new
+    end
   end
+
 
   private
 
